1d5228df7182317d2ca534a9120ee615b5b510be
Aymane dahi <aymane.dahi@etu.univ-amu.fr>
2025-06-17 11:01:30 +0200

    changement du zoom

--- code/zoommage.py
+++ code/zoommage.py
@@ -22,7 +22,7 @@ def zoom(chemin_fichier, option_nom_fichier):
         lignes = iter(lignes)
         lignes_zoomees = []
         index = -1
-        puissance_zoom = 150
+        puissance_zoom = 250
         for ligne in lignes:
             if "</ExtendedData>" in ligne:
                 index = len(lignes_zoomees)
